af2da9692d01f9ec14d61def74459f92b5668512
MariumMalek <marium.malek@g.bracu.ac.bd>
2022-11-25 23:53:43 +0600

    create_react_APP

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">medicine</a>
+      </header>
+      <main>List product</main>
+    </div>
+  );
+}
+
+export default App;
