f5d3baf46387fc56d3c6b3b7a3fce47bae18e6c8
MariumMalek <marium.malek@g.bracu.ac.bd>
2022-11-28 00:34:56 +0600

    product-listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">medicine</a>
       </header>
-      <main>List product</main>
+      <main>
+        <h1>Feature Product</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="products" key={product.napa}>
+              <a href={`/product/${product.napa} `}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className=" product-info">
+                <a href={`/product/${product.napa} `}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price} Tk</strong>{' '}
+                </p>
+                <button>Add cart </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      name: 'Napa',
+      price: 10,
+      instock: 100,
+      image: '/images/p1.jpg', 
+    },
+    {
+      name: 'Napa extra',
+      price: 15,
+      instock: 90,
+      image: '/images/p2.jpg',
+    },
+    {
+      name: 'Carticare',
+      price: 10,
+      instock: 100,
+      image: '/images/p3.jpg',
+    },
+  ],
+};
+
+export default data;
