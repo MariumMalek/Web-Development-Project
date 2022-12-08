fdf16bcefdf7ca9b532aac3eac36eb2e17c88ea9
MariumMalek <marium.malek@g.bracu.ac.bd>
2022-12-08 22:03:53 +0600

    product-listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">medicine</a>
-      </header>
-      <main>
-        <h1>Feature Product</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="products" key={product.napa}>
-              <a href={`/product/${product.napa} `}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className=" product-info">
-                <a href={`/product/${product.napa} `}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price} Tk</strong>{' '}
-                </p>
-                <button>Add cart </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">medicine</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -2,18 +2,21 @@ const data = {
   products: [
     {
       name: 'Napa',
+      slug: 'Napa',
       price: 10,
       instock: 100,
-      image: '/images/p1.jpg', 
+      image: '/images/p1.jpg',
     },
     {
       name: 'Napa extra',
+      slug: 'Napa-extra',
       price: 15,
       instock: 90,
       image: '/images/p2.jpg',
     },
     {
       name: 'Carticare',
+      slug: 'Carticare',
       price: 10,
       instock: 100,
       image: '/images/p3.jpg',
